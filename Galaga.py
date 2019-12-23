b4bb6ec2ba900a556a1938a969bdaa278b66b329
Eric Feldman <efeldm16@jhu.edu>
2019-12-23 17:33:33 -0500

    got basic alien animation working, need to work on fixing FPS to make alien animations better

--- Galaga.py
+++ Galaga.py
@@ -13,7 +13,7 @@ cwd = os.path.split(os.path.abspath(__file__))[0]
 class Player(pg.sprite.Sprite):
 
     velocity = 3
-    sprite_size = (16, 55, 16, 16) #x, y, x offset, y offset
+    sprite_size = (16, 55, 16, 16) # x, y, x offset, y offset
     num_sprites = 8
     images = []
 
@@ -23,7 +23,6 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
         self.rect.bottom = INITIAL_HEIGHT - 30
         self.reloading = 0
-
 
     def move(self, direction):
         self.rect.move_ip(direction * self.velocity, 0)
@@ -38,6 +37,11 @@ class Bullet(pg.sprite.Sprite):
     images = []
 
     def __init__(self, pos):
+        """
+        Initializes a bullet.
+        Args:
+            pos: midbottom of the bullet
+        """
         pg.sprite.Sprite.__init__(self, self.containers)
         self.image = self.images[0]
         self.rect = self.image.get_rect(midbottom=pos)
@@ -47,6 +51,63 @@ class Bullet(pg.sprite.Sprite):
         if self.rect.top <= 0:
             self.kill()
 
+class Alien(pg.sprite.Sprite):
+
+    velocity = 1
+    bounce = 1 # 1 for out, -1 for in
+    images = []
+    elapsed_time = 0
+    animation_time = 1000
+
+    def __init__(self, pos):
+        """
+        Initializes an alien.
+        Args:
+            pos: center corner of alien (x, y)
+        """
+        pg.sprite.Sprite.__init__(self, self.containers)
+        self.initial_pos = pos
+        self.image = self.images[-1]
+        self.rect = self.image.get_rect(center=pos)
+        self.waiting = True
+
+
+    def _waiting(self):
+        if self.bounce > 0:
+            self.image = self.images[-1]
+        else:
+            self.image = self.images[-2]
+
+        dir = self.bounce * self.velocity
+        if self.rect.centerx < (SCREENRECT.w / 2):
+            self.rect.move_ip(-dir, -dir)
+        else:
+            self.rect.move_ip(dir, -dir)
+
+    def _dive(self):
+        raise NotImplementedError
+
+    def update(self):
+        if self.waiting:
+            self._waiting()
+        else:
+            self._dive()
+
+    def change_bounce():
+        Alien.bounce = -Alien.bounce
+
+class BlueAlien(Alien):
+
+    sprites_info = [ \
+    (19, 177, 10, 13), \
+    (42, 178, 12, 11), \
+    (65, 176, 13, 14), \
+    (90, 177, 13, 13), \
+    (113, 176, 14, 13), \
+    (139, 177, 11, 12), \
+    (162, 178, 13, 10), \
+    (188, 178, 9, 10)]
+
 class Galaga:
 
     def __init__(self):
@@ -55,42 +116,55 @@ class Galaga:
         self.max_shots = 2
 
     def load_images(self):
-        #load player images
+        # open sprite file and initialize sprite sheet loader
         file = os.path.join(cwd, "sprites.png")
         ss = spritesheet(file)
+
+        # load player images
         Player.images = ss.rescale_strip(ss.load_strip(Player.sprite_size, Player.num_sprites, 8), SCALE)
 
-        #load bullet images
+        # load bullet images
         Bullet.images.append(ss.image_at((365, 219, 3, 8)))
         Bullet.images = ss.rescale_strip(Bullet.images, SCALE)
 
+        # load blue alien
+        BlueAlien.images = ss.rescale_strip(ss.images_at(BlueAlien.sprites_info), SCALE)
 
     def initialize_game(self):
         #initialize pygame
         pg.init()
 
-        self.clock = pg.time.Clock()
+        Galaga.clock = pg.time.Clock()
 
-        #initialize game window
+        # initialize game window
         self.window = pg.display.set_mode(SCREENRECT.size)
 
-        #load images
+        # load images
         self.load_images()
 
-        #create groups
+        # create groups
         self.all = pg.sprite.Group()
         self.bullets = pg.sprite.Group()
+        self.aliens = pg.sprite.Group()
 
-        #create containers
+        # create containers
         Player.containers = self.all
         Bullet.containers = self.all, self.bullets
+        Alien.containers = self.all, self.aliens
 
-        #create player
+        # create player
         self.player = Player()
+
+        # create aliens for testing
+        BlueAlien((SCREENRECT.w / 2 - 30, SCREENRECT.h / 2))
+        BlueAlien((SCREENRECT.w / 2 + 30, SCREENRECT.h / 2))
 
     def run(self):
         # main game loop
         while not self.game_over:
+            # manage time
+            dt = self.clock.tick(FPS)
+
             # handle events
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -109,6 +183,12 @@ class Galaga:
                 Bullet(self.player.get_gun_position())
             self.player.reloading = firing
 
+            # check which direction aliens should be moving in
+            Alien.elapsed_time += dt
+            if Alien.elapsed_time > Alien.animation_time:
+                Alien.elapsed_time = 0
+                Alien.change_bounce()
+
             # update all game objects
             self.all.update()
 
@@ -117,14 +197,13 @@ class Galaga:
             self.all.draw(self.window)
             pg.display.update()
 
-            #set FPS
-            self.clock.tick(FPS)
-
-
+    def quit(self):
+        pg.quit()
 
 def main():
     game = Galaga()
     game.run()
+    game.quit()
 
 if __name__ == "__main__":
     main()
